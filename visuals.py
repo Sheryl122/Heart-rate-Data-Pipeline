de4b53caae42e20ab3eb9cfa3b85e1b9b8ea8cb1
Sheryl122 <sherylboateng1@gmail.com>
2026-03-27 13:25:10 -0400

    Line color changed

--- visuals.py
+++ visuals.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def lineplot_image(data: list):
     fig, ax = plt.subplots()
 
-    ax.plot(data, color = "blue")
+    ax.plot(data, color = "purple")
     ax.set_title("Heart Rate Data Pipeline")
     ax.set_xlabel("Minutes")
     ax.set_ylabel("Heart Rate (bpm)")
